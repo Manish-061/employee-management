fc9aa17d5c3fda0736c3d27f4b315e76c797b30c
Manish-061 <kumarmanish092003@gmail.com>
2025-02-09 15:11:23 +0530

    Set Data snd Opening employee or admin.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { getLocalStorage, setLocalStorage } from './utils/localStorage'
+import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
 
-  // useEffect(() => {
-  //   setLocalStorage()
-  //   getLocalStorage()
-  // },)
-
   const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext)
+
 
   const handleLogin = (email, password) =>{
-    console.log(email, password);
-    
+    if(email == "admin@me.com" && password == '123'){
+     setUser('admin')
+    } else if (email == "user@me.com" && password == '123'){
+      setUser('employee')
+    }else{
+      alert("Invalid credentials.")
+    }
   }
-
 
   return (
     <> 
-        {!user ? <Login handleLogin={handleLogin} /> : ''}
-        {/* <EmployeeDashboard /> */}
-        {/* <AdminDashboard /> */}
+        {!user ? <Login handleLogin={handleLogin}/> : ''}
+        {user == 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   )
 }
